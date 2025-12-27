412effdde9310c66cc43840efe1a4c47be341372
N-Verse <49152837+ADustyCode@users.noreply.github.com>
2025-12-28 00:38:39 +0700

    Create TC04_Empty_Username.groovy

---
+++ TestCases/TC04_Empty_Username.groovy
@@ -0,0 +1,13 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.webui.driver.DriverFactory
+import org.openqa.selenium.By
+
+WebUI.openBrowser('')
+WebUI.navigateToUrl('http://zero.webappsecurity.com/login.html')
+
+DriverFactory.getWebDriver().findElement(By.xpath("//input[@id='user_password']")).sendKeys("password")
+DriverFactory.getWebDriver().findElement(By.xpath("//input[@name='submit']")).click()
+
+assert DriverFactory.getWebDriver().findElement(By.xpath("//div[contains(@class,'alert-error')]")).isDisplayed()
+
+//WebUI.closeBrowser()
