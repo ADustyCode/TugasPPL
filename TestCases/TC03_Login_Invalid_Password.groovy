dd58382f100d2c3686a8ab95fb5287e0d501f638
N-Verse <49152837+ADustyCode@users.noreply.github.com>
2025-12-28 00:38:11 +0700

    Create TC03_Login_Invalid_Password.groovy

---
+++ TestCases/TC03_Login_Invalid_Password.groovy
@@ -0,0 +1,12 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.webui.driver.DriverFactory
+import org.openqa.selenium.By
+
+WebUI.openBrowser('')
+WebUI.navigateToUrl('http://zero.webappsecurity.com/login.html')
+
+DriverFactory.getWebDriver().findElement(By.xpath("//input[@id='user_login']")).sendKeys("username")
+DriverFactory.getWebDriver().findElement(By.xpath("//input[@id='user_password']")).sendKeys("wrong")
+DriverFactory.getWebDriver().findElement(By.xpath("//input[@name='submit']")).click()
+
+assert DriverFactory.getWebDriver().findElement(By.xpath("//div[contains(@class,'alert-error')]")).isDisplayed()
